4bc7249ab80b0b39117f592b4aa46504b0b41691
Douglas <douglasmcht@gmail.com>
2024-11-22 16:59:34 -0600

    Update page.tsx

---
+++ app/dashboard/category/create/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+
+import CategoryCreate from "@/components/category/category-create";
+
+export default function Clients() {
+    return (
+        <div className="p-6">
+          <CategoryCreate/>
+        </div>
+      );
+}
