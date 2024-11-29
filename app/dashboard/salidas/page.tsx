9020b2266f3993e5e1425ece7fe12de4ffc7e1a6
Ludwin Blanco <lblancoorellana@gmail.com>
2024-11-29 09:27:15 -0600

    resolucion de conflictos 2

--- app/dashboard/salidas/page.tsx
+++ app/dashboard/salidas/page.tsx
@@ -12,14 +12,15 @@ import CamposIndex from "@/components/salidas/salidas-index";
 export default function Campo() {
   const { user } = useSession();
 
-  if (!user) {
-    return <div>Loading...</div>;
-  }
-
+  
   const { data: campo, isLoading } = useQuery({
     queryKey: ["campo", user?.id],
     queryFn: () => getSalidas(),
   });
+
+  if (!user) {
+    return <div>Loading...</div>;
+  }
 
   if (user && !rolesPermissions.access_clients_index.includes(user?.profile.rol_id)) {
     return <NotAllowed />;
