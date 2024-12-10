26a22bd1e15dcd61bf61be4e0887f086705186e2
Ludwin Blanco <lblancoorellana@gmail.com>
2024-12-10 14:03:08 -0600

    generacion de qr

--- app/dashboard/users/absences/create/page.tsx
+++ app/dashboard/users/absences/create/page.tsx
@@ -2,12 +2,27 @@
 import { useSession } from "@/app/session-provider";
 import AbsencesCreate from "@/components/ausencias/create";
 import { getAbsenceCategories } from "@/services/absences";
+import { useEffect, useState } from "react";
+import { AbsenceCategory } from "@/types/models";
 
 
-const AbsencesPage = async () => {
+const AbsencesPage = () => {
     const { user } = useSession();
 
-    const categories = await getAbsenceCategories();
+    const [categories, setCategories] = useState<AbsenceCategory[]>([]);
+
+    useEffect(() => {
+      const fetchCategories = async () => {
+        try {
+          const result = await getAbsenceCategories();
+          setCategories(result ?? []);
+        } catch (error) {
+          console.error("Error fetching categories:", error);
+        }
+      };
+  
+      fetchCategories();
+    }, []);
 
   return (
     <AbsencesCreate categories={categories ?? []}/>
